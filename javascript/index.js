8f8cc39037ace2611bdc0d0571753fd69cd00373
Marian Caron <caron.marian@gmail.com>
2020-09-21 23:34:02 +0200

    add simple dark mode function

---
+++ javascript/index.js
@@ -0,0 +1,18 @@
+// Select the button
+const btn = document.querySelector(".toggle-button");
+console.log(btn);
+// Select the stylesheet <link>
+const theme = document.querySelector("#theme-link");
+
+// Listen for a click on the button
+btn.addEventListener("click", function() {
+  // If the current URL contains "ligh-theme.css"
+  if (theme.getAttribute("href") == "stylesheets/light-theme.css") {
+    // ... then switch it to "dark-theme.css"
+    theme.href = "stylesheets/dark-theme.css";
+  // Otherwise...
+  } else {
+    // ... switch it to "light-theme.css"
+    theme.href = "stylesheets/light-theme.css";
+  }
+});
